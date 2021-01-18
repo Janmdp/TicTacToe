a0c5df90882d76026e232efd3a5e0a4fc02252a0
Jan Eickmans <jan.eickmans@hotmail.nl>
2021-01-18 08:06:48 +0100

    Fixed pasword encryption

--- tictactoeapi/src/main/java/com/example/tictactoeapi/Controllers/UserController.java
+++ tictactoeapi/src/main/java/com/example/tictactoeapi/Controllers/UserController.java
@@ -4,11 +4,10 @@ import classes.User;
 import com.example.tictactoeapi.Models.UserModel;
 import com.example.tictactoeapi.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,31 +15,26 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService service;
-
     @GetMapping("/users")
     public List<User> list(){
         return service.listAll();
     }
 
+
+
     @GetMapping("/user/{id}")
     public User getById(@PathVariable Integer id){
         return service.get(id);
     }
+
     @GetMapping("/user")
     public User login(@RequestParam String name, @RequestParam String password){
-        return service.login(name,bCryptPasswordEncoder.encode(password));
+        return service.login(name,password);
     }
     @PostMapping("/user")
-    public void register(@RequestParam String name, @RequestParam String password){
-        service.register(name,bCryptPasswordEncoder.encode(password));
+    public void register(@RequestParam String name, @RequestParam String password) {
+        service.register(name,password);
     }
-    @PostMapping("/authenticate")
-   public String generateToken(@RequestBody AuthRequest authRequest){
-        //authenticationManager.authenticate(
-       //         new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
-        //);
-        return "empty";
-   }
 
 
 }
--- tictactoeapi/src/main/java/com/example/tictactoeapi/Services/UserService.java
+++ tictactoeapi/src/main/java/com/example/tictactoeapi/Services/UserService.java
@@ -4,6 +4,7 @@ import classes.User;
 import com.example.tictactoeapi.Models.UserModel;
 import com.example.tictactoeapi.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,6 +15,8 @@ public class UserService {
 
     @Autowired
     private UserRepository repo;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     public List<User> listAll(){
         List<User> toReturn = new ArrayList<>();
@@ -41,7 +44,7 @@ public class UserService {
     public User login(String name, String password){
         User user = new User();
         for (UserModel model: repo.findAll()){
-            if(model.getName().equals(name) && model.getPassword().equals(password)){
+            if(model.getName().equals(name) && bCryptPasswordEncoder.matches(password, model.getPassword())){
                 user = new User(model.getId(), model.getName(), model.getPassword());
                 return user;
             }
@@ -52,7 +55,7 @@ public class UserService {
     public void register(String name, String password){
         UserModel model = new UserModel();
         model.setName(name);
-        model.setPassword(password);
+        model.setPassword(bCryptPasswordEncoder.encode(password));
         repo.save(model);
     }
 }
